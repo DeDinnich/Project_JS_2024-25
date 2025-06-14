9c83dbc95eedce1736889c36a600405e4c233809
DeDinnich <lucas.dinnichert.pro@gmail.com>
2025-06-14 19:57:38 +0200

    header fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,12 @@
-<header class="position-relative text-center text-warning" style="background-image: url('{{ asset('images/bg-header2.png') }}'); background-size: cover; height: 12vh;">
-    <div class="position-absolute top-0 start-0 w-100 h-100"></div>
-
-    <div class="position-relative z-1 d-flex h-100 align-items-center justify-content-center" data-aos="fade-down">
-        <i class="fas fa-book fa-2x me-3"></i>
-        <h1 class="fw-bold mb-0" style="font-size: 1.5rem;">
-            La bibliothèque du JS
-        </h1>
-    </div>
+<!-- HTML -->
+<header class="site-header text-center text-warning">
+  <div class="overlay"></div>
+  <div
+    class="header-content d-flex align-items-center justify-content-center"
+    data-aos="fade-down"
+  >
+    <i class="fas fa-book fa-2x me-3"></i>
+    <h1 class="fw-bold mb-0">La bibliothèque du JS</h1>
+  </div>
 </header>
+<div class="header-spacer"></div>
